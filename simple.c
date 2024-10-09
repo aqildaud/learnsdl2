696ffbca0e83020765d831517a8f92efa747ce81
salmah <aqildaud611@gmail.com>
2024-10-09 19:26:58 +0800

    update window title

--- simple.c
+++ simple.c
@@ -13,7 +13,7 @@ int init(SDL_Window** window, SDL_Surface** screenSurface) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     success = 1;
   } else {
-   *window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+   *window = SDL_CreateWindow("my reaction to that information", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (*window == NULL) { 
       printf("Window could not be created SDL_Error: %s\n", SDL_GetError());
       success = 1;
